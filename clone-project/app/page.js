63836e8f3f3cd3fbea8c38e1424e45dbe3bfe91e
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-04-21 21:55:27 +0530

    added navbar component in app

--- clone-project/app/page.js
+++ clone-project/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return(
+  return (
     <main>
-      <h1>Accredian enterprise - clone</h1>
+      <Navbar />
     </main>
   );
 }
